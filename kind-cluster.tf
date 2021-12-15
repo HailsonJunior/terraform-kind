6f3e569218f8e69df3cdea2851a3e69fe066fe46
hailson.junior <hailson.fsj@gmail.com>
2021-12-15 17:24:47 -0300

    Adjusts

--- kind-cluster.tf
+++ kind-cluster.tf
@@ -11,17 +11,9 @@ resource "kind_cluster" "giropops-cluster" {
     node {
       role = "control-plane"
 
-#      kubeadm_config_patches = [
-#        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
-#      ]
-#      extra_port_mappings {
-#        container_port = 80
-#        host_port      = 80
-#      }
-#      extra_port_mappings {
-#        container_port = 443
-#        host_port      = 443
-#      }
+      kubeadm_config_patches = [
+        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+      ]
     }
 
     node {
